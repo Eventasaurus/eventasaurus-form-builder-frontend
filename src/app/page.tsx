e902bb4167a9fa223b9e86038eb466923b2dbf79
Mayank Pal <palmayank2100@gmail.com>
2025-09-25 15:12:11 +0530

    add Prettier configuration and format codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import {Button} from "@/components/ui/button";
-import {ModeToggle} from "@/components/mode-toggle";
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/mode-toggle';
 
 export default function Home() {
   return (
     <div>
       <Button>Hello World</Button>
-      <ModeToggle/>
+      <ModeToggle />
     </div>
   );
 }
